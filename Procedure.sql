0ca2842935e3e236556b5b8fe2cd8524d4db38b5
Egor Fedorenko <120181389+GRAADIUS@users.noreply.github.com>
2023-10-27 14:16:06 +0300

    Update Procedure.sql

--- Procedure.sql
+++ Procedure.sql
@@ -9,3 +9,22 @@ CREATE PROCEDURE Add_Patsient
 AS
   INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) 
   VALUES (@perenimi, @nimi, @sünnipäev, @diagnoos, @vastuvõtukuupäev, @väljalaskekuupäev, @Osakonna_ID);
+
+EXEC Add_Patsient @perenimi = 'Gregotov', @nimi = 'Marjhus',
+@sünnipäev = '1980-02-25', @diagnoos = 'Skisofreenia',
+@vastuvõtukuupäev = '2020-05-16', @väljalaskekuupäev = '2021-07-21',
+@Osakonna_ID = 1;
+select * from Patsient;
+
+
+CREATE PROCEDURE Konsultatioon
+  @Patsiendi int,
+  @Asti int,
+  @konsultatsioon_kellaaeg datetime,
+  @põhjuse varchar(50)
+AS
+INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) 
+VALUES (@Patsiendi, @Asti, @konsultatsioon_kellaaeg, @põhjuse);
+
+EXEC Konsultatioon @Patsiendi = 4, @Asti = 3, @konsultatsioon_kellaaeg = '2023-10-25 09:00:00', @põhjuse = 'Ärevus'
+select * from terapeudi_konsultatsioon;
