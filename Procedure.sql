fa36501823d521b49fd9fcf6174c0e7b4e00f19c
Egor Fedorenko <120181389+GRAADIUS@users.noreply.github.com>
2023-10-27 13:42:54 +0300

    Update Procedure.sql

--- Procedure.sql
+++ Procedure.sql
@@ -1,12 +1,11 @@
-CREATE PROCEDURE Add_Patsient()
-  @perenimi varchar(20);
-  @nimi varchar(20);
-  @sünnipäev date;
-  @diagnoos varchar(50);
-  @vastuvõtukuupäev date;
-  @väljalaskekuupäev date;
-  @Osakonna_ID int;
-BEGIN
+CREATE PROCEDURE Add_Patsient
+  @perenimi varchar(20),
+  @nimi varchar(20),
+  @sünnipäev date,
+  @diagnoos varchar(50),
+  @vastuvõtukuupäev date,
+  @väljalaskekuupäev date,
+  @Osakonna_ID int
+AS
   INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) 
   VALUES (@perenimi, @nimi, @sünnipäev, @diagnoos, @vastuvõtukuupäev, @väljalaskekuupäev, @Osakonna_ID);
-END$$
