08e3f74bb6228328558f7d13cb0ba483328bc147
Egor Fedorenko <120181389+GRAADIUS@users.noreply.github.com>
2023-11-01 14:34:36 +0200

    Update insert_xampp.sql

--- insert_xampp.sql
+++ insert_xampp.sql
@@ -1,3 +1,4 @@
+-- Insert events into the Ajakava table
 INSERT INTO Ajakava(Sundmus, Algusaeg, Lopuaeg) VALUES ('Hommikusöök', '2023-10-12 08:00:00', '2023-10-12 09:00:00');
 INSERT INTO Ajakava(Sundmus, Algusaeg, Lopuaeg) VALUES ('Kõndima', '2023-10-12 09:00:00', '2023-10-12 10:00:00');
 INSERT INTO Ajakava(Sundmus, Algusaeg, Lopuaeg) VALUES ('Vaba aeg', '2023-10-12 10:00:00', '2023-10-12 11:00:00');
@@ -9,6 +10,7 @@ INSERT INTO Ajakava(Sundmus, Algusaeg, Lopuaeg) VALUES ('Tuled kustunud', '2023-
 INSERT INTO Ajakava(Sundmus, Algusaeg, Lopuaeg) VALUES ('Hommikusöök', '2023-10-13 08:00:00', '2023-10-13 09:00:00');
 INSERT INTO Ajakava(Sundmus, Algusaeg, Lopuaeg) VALUES ('Kõndima', '2023-10-13 09:00:00', '2023-10-13 10:00:00');
 
+-- Insert job titles into the Toohive table
 INSERT INTO Toohive(Toohive) VALUES ('Arst');
 INSERT INTO Toohive(Toohive) VALUES ('Meditsiiniõde');
 INSERT INTO Toohive(Toohive) VALUES ('Korravalvuri');
@@ -16,79 +18,7 @@ INSERT INTO Toohive(Toohive) VALUES ('Psühhoterapeut');
 INSERT INTO Toohive(Toohive) VALUES ('Haigla juhataja');
 INSERT INTO Toohive(Toohive) VALUES ('Intern');
 
+-- Insert departments into the Osakonna table
 INSERT INTO Osakonna(Osakonna_nimi, Asukoht) VALUES ('Põhja-Eesti Regionaalhaigla Seewaldi psühhiaatriapolikliinik', 'Paldiski mnt 52-6, 10614 Tallinn');
 INSERT INTO Osakonna(Osakonna_nimi, Asukoht) VALUES ('PERH Psühhiaatriakliinik', 'Paldiski mnt 52, 10614 Tallinn');
 INSERT INTO Osakonna(Osakonna_nimi, Asukoht) VALUES ('Vaimuhaigla 1.korpus', 'Wismari 13, 10136 Tallinn');
-
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('sertraliin', 'Antidepressant', 'unisus, suukuivus, kehakaalu muutused, seksuaalprobleemid');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('fluoksetiin', 'Antidepressant', 'unisus, suukuivus, kehakaalu muutused, seksuaalprobleemid');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('estsitalopraami', 'Antidepressant', 'unisus, suukuivus, kehakaalu muutused, seksuaalprobleemid');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('paroksetiin', 'Antidepressant', 'unisus, suukuivus, kehakaalu muutused, seksuaalprobleemid');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('risperidoon', 'Antipsühhootikum', 'unisus, liikumisprobleemid, kehakaalu tõus, madal vererõhk');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('aripiprasool', 'Antipsühhootikum', 'unisus, liikumisprobleemid, kehakaalu tõus, madal vererõhk');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('kvitiapiin', 'Antipsühhootikum', 'unisus, liikumisprobleemid, kehakaalu tõus, madal vererõhk');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('olansapiin', 'Antipsühhootikum', 'unisus, liikumisprobleemid, kehakaalu tõus, madal vererõhk');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('alprasolaam', 'Anksiolüütiline', 'unisus, koordinatsioonihäired, sõltuvus');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('diasepaam', 'Anksiolüütiline', 'unisus, koordinatsioonihäired, sõltuvus');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('lorasepaam', 'Anksiolüütiline', 'unisus, koordinatsioonihäired, sõltuvus');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('liitium', 'Normotikumi', 'kehakaalu muutused, värisemine, kilpnäärme probleemid.');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('valproehape', 'Normotikumi', 'kehakaalu muutused, värisemine, kilpnäärme probleemid.');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('lamotrigiin', 'Normotikumi', 'kehakaalu muutused, värisemine, kilpnäärme probleemid.');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('zolpideem', 'Hüpnootiline', 'uimasus, sõltuvus, peavalu.');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('estsitopraam', 'Hüpnootiline', 'uimasus, sõltuvus, peavalu.');
-INSERT INTO Ravim(Ravimi_nimi, Eesmärk, Korvalmõjud) VALUES ('trasodoon', 'Hüpnootiline', 'uimasus, sõltuvus, peavalu.');
-
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Shurik', 'Largonec', 1, 'E: 08:00 - 19:00, T: 08:00 - 19:00, N: 08:00 - 19:00', '+37250947581');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Larry', 'Kognec', 2, 'E: 08:00 - 19:00, T: 08:00 - 19:00, N: 08:00 - 19:00', '+3725180944');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Anna', 'Gortra', 5, 'E: 08:00 - 19:00, T: 08:00 - 19:00, K: 08:00 - 19:00', '+37250984068');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Fejah', 'Muren', 2, 'E: 08:00 - 19:00, T: 08:00 - 19:00, K: 08:00 - 19:00', '+37250649250');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('John', 'Tuzaros', 6, 'E: 08:00 - 19:00, T: 08:00 - 19:00, N: 08:00 - 19:00', '+37256203025');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Olav', 'Lagtry', 4, 'E: 08:00 - 19:00, T: 08:00 - 19:00, N: 08:00 - 19:00', '+3725007885214');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Sarrah', 'Santion', 1, 'E: 08:00 - 19:00, T: 08:00 - 19:00, N: 08:00 - 19:00', '+37250008795');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Luigy', 'Drend', 5, 'E: 08:00 - 19:00, T: 08:00 - 19:00, K: 08:00 - 19:00', '+37259698787');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Syuzy', 'Montu', 2, 'E: 08:00 - 19:00, N: 08:00 - 19:00, E: 08:00 - 19:00', '+37255555555');
-INSERT INTO Tootajad(Nimi, Perekonnanimi, Spetsialiseerumine, Too_algus, Kontaktinfo) VALUES ('Mated', 'Lefti', 3, 'E: 08:00 - 19:00, T: 08:00 - 19:00, N: 08:00 - 19:00', '+372509998748');
-
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Petrova', 'Jelena', '1990-05-12', 'Depressiivne häire', '2018-08-20', '2019-02-15', 1);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Ivanov', 'Aleksei', '1982-09-28', 'Skisofreenia', '2017-03-10', '2018-01-05', 1);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Sidorova', 'Marina', '1976-12-03', 'Bipolaarne meeleoluhäire', '2019-11-14', '2020-09-22', 1);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Kozlov', 'Andrei', '1995-04-18', 'Paranoiline skisofreenia', '2016-07-03', '2017-05-29', 2);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Nikolaeva', 'Olga', '1987-08-25', 'Ärevushäire', '2020-02-12', '2021-01-08', 2);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Grigorjev', 'Sergei', '1993-03-30', 'Obsessiiv-kompulsiivne häire', '2015-10-17', '2016-06-20', 2);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Lebedeva', 'Natalja', '1980-11-09', 'Traumajärgne stressihäire', '2018-04-05', '2019-03-01', 2);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Smirnov', 'Dmitri', '1998-06-15', 'Skisotüüpne isiksushäire', '2017-09-26', '2018-08-12', 3);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Morozova', 'Julia', '1984-01-07', 'Buliimia', '2019-07-19', '2020-06-14', 3);
-INSERT INTO Patsient(Perekonnanimi, Nimi, Sünnikuupäev, Diagnoos, vastuvõtukuupäev, väljalaskekuupäev, Osakonna) VALUES ('Fedorov', 'Igor', '1991-07-22', 'Dissotsiatiivne isiksushäire', '2016-12-08', '2017-10-03', 3);
-
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (1, 2, 3, '2023-10-25', 'Hea');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (2, 1, 4, '2023-10-26', 'Paranemisel');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (3, 3, 2, '2023-10-27', 'Stabiilne');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (4, 1, 1, '2023-10-28', 'Halvenemine');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (5, 2, 3, '2023-10-29', 'Hea');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (6, 3, 4, '2023-10-30', 'Paranemisel');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (7, 1, 2, '2023-10-31', 'Stabiilne');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (8, 2, 1, '2023-11-01', 'Halvenemine');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (9, 3, 3, '2023-11-02', 'Hea');
-INSERT INTO Ravi(Patsiendi, Arsti, Määratud_ravimid, Ravi_kestus, Ravi_tulemused) VALUES (10, 1, 4, '2023-11-03', 'Paranemisel');
-
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (1, 2, '2023-10-25 09:00:00', 'Ärevus');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (2, 1, '2023-10-26 14:30:00', 'Depressioon');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (3, 3, '2023-10-27 11:15:00', 'Suhteprobleemid');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (4, 1, '2023-10-28 16:45:00', 'Ärevus');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (5, 2, '2023-10-29 10:30:00', 'Depressioon');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (6, 3, '2023-10-30 14:00:00', 'Suhteprobleemid');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (7, 1, '2023-10-31 09:30:00', 'Ärevus');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (8, 2, '2023-11-01 15:15:00', 'Depressioon');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (9, 3, '2023-11-02 12:00:00', 'Suhteprobleemid');
-INSERT INTO terapeudi_konsultatsioon(Patsiendi, Arsti, konsultatsioon_kellaaeg, põhjuse) VALUES (10, 1, '2023-11-03 17:00:00', 'Ärevus');
-
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (1, 2, '2023-10-25 10:00:00', 'Individuaalne teraapia', 'Paraneb');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (2, 1, '2023-10-26 15:30:00', 'Paariteraapia', 'Stabiilne');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (3, 3, '2023-10-27 12:15:00', 'Individuaalne teraapia', 'Paranemisel');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (4, 1, '2023-10-28 17:45:00', 'Rühmateraapia', 'Halvenemine');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (5, 2, '2023-10-29 11:30:00', 'Individuaalne teraapia', 'Paraneb');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (6, 3, '2023-10-30 15:00:00', 'Rühmateraapia', 'Stabiilne');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (7, 1, '2023-10-31 10:30:00', 'Individuaalne teraapia', 'Paranemisel');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (8, 2, '2023-11-01 16:15:00', 'Paariteraapia', 'Halvenemine');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (9, 3, '2023-11-02 13:00:00', 'Individuaalne teraapia', 'Stabiilne');
-INSERT INTO Psühhoteraapia_seanss(Patsiendi, Psühhoterapeudi, Seansi_kuupaev, Seansi_kirjeldus, Patsiendi_areng) VALUES (10, 1, '2023-11-03 18:00:00', 'Rühmateraapia', 'Paraneb');
